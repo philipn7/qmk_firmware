f7665fac94f48fa63b74b0ac61a475d8f3aa7a15
philipn7 <philipn7@gmail.com>
2021-10-10 10:53:47 -0400

    moved mouse buttons to bottom row

--- keyboards/handwired/dactyl_manuform/5x7/keymaps/philipn7/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x7/keymaps/philipn7/keymap.c
@@ -4,7 +4,7 @@
 #define U_NA KC_NO // present but not available for use
 #define U_NU KC_NO // available but not used
 
-enum layers { BASE, NAV, SYM, NUM, FUN };
+enum layers { BASE, NAV, SYM, NUM, FUN, RST };
 
 #define U_RDO C(KC_Y)
 #define U_PST C(KC_V)
@@ -21,18 +21,18 @@ enum layers { BASE, NAV, SYM, NUM, FUN };
 #define SUP_R   LGUI(KC_RIGHT)
 
 enum keycodes {
-  G_SCRL = SAFE_RANGE,
+  SCRL_WHL = SAFE_RANGE,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [BASE] = LAYOUT_5x7(
   // left hand
-   KC_ESC,    KC_Q,         KC_W,    KC_F,   KC_P,   KC_B,   KC_NUMLOCK,
-   KC_LCTL,   KC_A,         KC_R,    KC_S,   KC_T,   KC_G,   TO(NAV),
+   KC_ESC,    KC_Q,         KC_W,    KC_F,   KC_P,   KC_B,   MO(RST),
+   KC_LCTL,   KC_A,         KC_R,    KC_S,   KC_T,   KC_G,   U_NA,
    KC_LSHIFT, KC_Z,         KC_X,    KC_C,   KC_D,   KC_V,
-   KC_LALT,   KC_LGUI,      TAB_L,   TAB_R,
-                                      KC_SPC,   LT(NAV,KC_HOME),   KC_END,
+   SCRL_WHL,   KC_BTN3,      KC_BTN2,   KC_BTN1,
+                                         LT(NAV,KC_SPC),   KC_HOME,   KC_END,
                                                 KC_TAB,   KC_LALT,
         // right hand
                      KC_BTN1,    KC_J,    KC_L,    KC_U,     KC_Y,     KC_QUOT,     KC_DEL,
@@ -44,26 +44,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [NAV] = LAYOUT_5x7(
   // left hand
-   RESET,    U_NA,        U_NA,    U_NA,   U_NA,   U_NA,   KC_NUMLOCK,
-   KC_LCTL,   U_NA,       U_CUT,   U_CPY,  U_PST,  G_SCRL,   TO(BASE),
+   KC_ESC,    U_NA,        U_NA,    U_NA,   U_NA,   U_NA,   KC_NUMLOCK,
+   KC_LCTL,   U_NA,       U_CUT,   U_CPY,  U_PST,  U_NA,   U_NA,
    KC_LSHIFT, U_UND,      U_RDO,    U_NA,   U_NA,   U_NA,
-   KC_LALT,   KC_LGUI,      TAB_L,   TAB_R,
+   U_NA,   U_NA,      U_NA,    U_NA,
                                       U_NA,   U_NA,   U_NA,
                                                 KC_LGUI,   KC_LALT,
         // right hand
-                     KC_BTN1,    KC_INS,  KC_HOME, KC_UP,   KC_END,  U_NA,     KC_DEL,
+                     KC_BTN1,    KC_BTN3,  KC_HOME, KC_UP,   KC_END,  U_NA,     KC_DEL,
                       KC_BTN2,   U_NA,    KC_LEFT, KC_DOWN, KC_RGHT, U_NA,     KC_ENT,
-                                 U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    KC_CAPS,
+                                 KC_INS,    U_NA,    U_NA,    U_NA,    U_NA,    KC_CAPS,
                                                  KC_LEFT,   KC_UP,   KC_DOWN,  KC_RGHT,
         U_NA,    U_NA,    KC_BSPC,
         KC_PGUP, KC_PGDN ),
 
 [SYM] = LAYOUT_5x7(
   // left hand
-   KC_ESC,    KC_LCBR, KC_EXLM, KC_AT,  KC_RCBR, KC_PIPE,   KC_NUMLOCK,
-   KC_LCTL,   KC_LPRN, KC_HASH, KC_DLR,   KC_RPRN, KC_GRV,   TO(NAV),
+   KC_ESC,    KC_LCBR, KC_EXLM, KC_AT,  KC_RCBR, KC_PIPE,   U_NA,
+   KC_LCTL,   KC_LPRN, KC_HASH, KC_DLR,   KC_RPRN, KC_GRV,   U_NA,
    KC_LSHIFT, KC_LBRC, KC_PERC, KC_CIRC,   KC_RBRC, KC_TILD, 
-   KC_LALT,   KC_LGUI, TAB_L,   TAB_R,
+   U_NA,   U_NA,      U_NA,    U_NA,
                                       KC_SPC,   KC_UNDS, KC_DOT,
                                                 KC_TAB,   KC_LALT,
         // right hand
@@ -76,10 +76,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [NUM] = LAYOUT_5x7(
   // left hand
-   KC_ESC,    KC_LCBR, KC_EXLM, KC_AT,  KC_RCBR, KC_PIPE,   KC_NUMLOCK,
-   KC_LCTL,   KC_LPRN, KC_HASH, KC_DLR,   KC_RPRN, KC_GRV,   TO(NAV),
+   KC_ESC,    KC_LCBR, KC_EXLM, KC_AT,  KC_RCBR, KC_PIPE,   U_NA,
+   KC_LCTL,   KC_LPRN, KC_HASH, KC_DLR,   KC_RPRN, KC_GRV,   U_NA,
    KC_LSHIFT, KC_LBRC, KC_PERC, KC_CIRC,   KC_RBRC, KC_TILD,
-   KC_LALT,   KC_LGUI, TAB_L,   TAB_R,
+   U_NA,   U_NA,      U_NA,    U_NA,
                                       KC_SPC,   KC_UNDS,   KC_DOT,
                                                 KC_TAB,   KC_LALT,
         // right hand
@@ -92,10 +92,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [FUN] = LAYOUT_5x7(
   // left hand
-   KC_ESC,    U_NA,    U_NA, U_NA,    U_NA,    U_NA,   KC_NUMLOCK,
-   KC_LCTL,   U_NA,    U_NA, U_NA,    U_NA,    U_NA,   TO(NAV),
+   KC_ESC,    U_NA,    U_NA, U_NA,    U_NA,    U_NA,   U_NA,
+   KC_LCTL,   U_NA,    U_NA, U_NA,    U_NA,    U_NA,   U_NA,
    KC_LSHIFT, U_NA,    U_NA, U_NA,    U_NA,    U_NA,
-   KC_LALT,   KC_LGUI,      U_NA,    U_NA,
+   U_NA,   U_NA,      U_NA,    U_NA,
                                       U_NA,    U_NA,    U_NA,
                                                 U_NA,   KC_LALT,
         // right hand
@@ -103,19 +103,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                       KC_BTN2,   KC_SLCK, KC_F4,   KC_F5,   KC_F6,   KC_F11,     KC_ENT,
                                  KC_PAUS, KC_F1,   KC_F2,   KC_F3,   KC_F10,  KC_CAPS,
                                                  KC_LEFT, KC_UP,    KC_DOWN,  KC_RGHT,
+        U_NA,     U_NA,    U_NA,
+        U_NA, U_NA ),
+
+[RST] = LAYOUT_5x7(
+  // left hand
+   RESET,    U_NA,    U_NA, U_NA,    U_NA,    U_NA,   U_NA,
+   U_NA,   U_NA,    U_NA, U_NA,    U_NA,    U_NA,   U_NA,
+   U_NA, U_NA,    U_NA, U_NA,    U_NA,    U_NA,
+   U_NA,   U_NA,      U_NA,    U_NA,
+                                      U_NA,    U_NA,    U_NA,
+                                                U_NA,   U_NA,
+        // right hand
+                     U_NA,    U_NA,    U_NA, U_NA,    U_NA,    U_NA,   U_NA,
+                      U_NA,    U_NA,    U_NA, U_NA,    U_NA,    U_NA,   U_NA,
+                                 U_NA, U_NA,    U_NA, U_NA,    U_NA,    U_NA,
+                                                 U_NA,   U_NA,      U_NA,    U_NA,
         U_NA,    U_NA,    U_NA,
         U_NA, U_NA )
 
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  static uint16_t my_hash_timer;
+  //static uint16_t my_hash_timer;
   switch (keycode) {
-    case G_SCRL:
-      // Hold for numlock, key g otherwise
+    case SCRL_WHL:
+      // Hold for numlock, off otherwise
       if (record->event.pressed) {
         // Do something when pressed 
-        my_hash_timer = timer_read();
+        //my_hash_timer = timer_read();
         
         if (!host_keyboard_led_state().num_lock) {
           tap_code(KC_NUMLOCK);
@@ -125,9 +141,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         if (host_keyboard_led_state().num_lock) {
           tap_code(KC_NUMLOCK);
         }
-        if (timer_elapsed(my_hash_timer) < TAPPING_TERM) {
-          tap_code(KC_G);
-        }
+        //if (timer_elapsed(my_hash_timer) < TAPPING_TERM) {
+          // if not held then ?
+        //}
       }
       return false; // Skip all further processing of this key
     default:
