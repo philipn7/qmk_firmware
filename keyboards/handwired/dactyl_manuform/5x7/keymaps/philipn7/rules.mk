3dc6a0013aa8a3d0f09acb999ae974bb41a24431
philipn7 <philipn7@gmail.com>
2021-10-11 21:18:54 -0400

    replaced 5x7 from master

--- keyboards/handwired/dactyl_manuform/5x7/keymaps/philipn7/rules.mk
+++
@@ -1,7 +0,0 @@
-MOUSEKEY_ENABLE = yes # Mouse keys
-EXTRAKEY_ENABLE = no # Audio control and System control
-AUTO_SHIFT_ENABLE = no # Auto Shift
-TAP_DANCE_ENABLE = yes
-COMBO_ENABLE = no
-
-OLED_DRIVER_ENABLE = no
--- keyboards/handwired/dactyl_manuform/5x7/keymaps/via/config.h
+++ keyboards/handwired/dactyl_manuform/5x7/keymaps/via/config.h
@@ -1,6 +1,5 @@
 /*
 Copyright 2012 Jun Wako <wakojun@gmail.com>
-Copyright 2015 Jack Humbert
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -18,23 +17,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#include "config_common.h"
+#define USE_SERIAL
 
-#define PRODUCT_ID 0x3537
-#define DEVICE_VER 0x0001
-#define PRODUCT    Dactyl-Manuform (5x7)
+#define MASTER_LEFT
+//#define MASTER_RIGHT
 
-/* key matrix size */
-// Rows are doubled-up
-#define MATRIX_ROWS 12
-#define MATRIX_COLS 7
-
-// wiring of each half
-#define MATRIX_ROW_PINS { D4, C6, D7, E6, B4, B5 }
-#define MATRIX_COL_PINS { F5, F6, F7, B1, B3, B2, B6 }
-
-#define DIODE_DIRECTION COL2ROW
-
-// WS2812 RGB LED strip input and number of LEDs
-#define RGB_DI_PIN D3
-#define RGBLED_NUM 12
+//#define EE_HANDS
